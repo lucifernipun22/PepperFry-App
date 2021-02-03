009615abbabd8b845bda99a48ac50f79681ef5e7
Nipun Jain <lucifernipun22@gmail.com>
2021-02-03 18:11:07 +0530

    "App"

---
+++ app/src/main/java/com/example/DepartmentSection/ClearanceSaleExtended.java
@@ -0,0 +1,16 @@
+package com.example.DepartmentSection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pepperfry.R;
+
+public class ClearanceSaleExtended extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clearance_sale_extended);
+    }
+}
