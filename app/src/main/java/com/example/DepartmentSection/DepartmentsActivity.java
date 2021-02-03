3a202ec07990f4392f1d1cc00f56bcbe47b03a68
MINTU325 <sainimintu34@gmail.com>
2021-02-03 11:45:18 +0530

    Whole Department Section

---
+++ app/src/main/java/com/example/DepartmentSection/DepartmentsActivity.java
@@ -0,0 +1,107 @@
+package com.example.DepartmentSection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.pepperfry.R;
+
+public class DepartmentsActivity extends AppCompatActivity implements View.OnClickListener {
+
+private Button mBTNfurniture;
+private Button mBTNLiving;
+private Button mBTNBedRoom;
+private Button mBTNKidsRoom;
+private Button mBTNMattresses;
+private Button mBTNFurnishings;
+private Button mBTNDecor;
+private Button mBTNLightings;
+private Button mBTNModularFurniture;
+private Button mBTNClearanceSale;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_departments);
+        initViews();
+
+    }
+
+    private void initViews() {
+        mBTNfurniture = findViewById(R.id.btnfurniture);
+        mBTNLiving = findViewById(R.id.btnLiving);
+        mBTNBedRoom = findViewById(R.id.btnBedRoom);
+        mBTNKidsRoom = findViewById(R.id.btnKidsRoom);
+        mBTNMattresses = findViewById(R.id.btnMattresses);
+        mBTNFurnishings = findViewById(R.id.btnFurnishings);
+        mBTNDecor = findViewById(R.id.btnDecor);
+        mBTNLightings = findViewById(R.id.btnLightings);
+        mBTNModularFurniture = findViewById(R.id.btnModularFurniture);
+        mBTNClearanceSale = findViewById(R.id.btnClearanceSale);
+
+        mBTNfurniture.setOnClickListener(this);
+        mBTNLiving.setOnClickListener(this);
+        mBTNBedRoom.setOnClickListener(this);
+        mBTNKidsRoom.setOnClickListener(this);
+        mBTNMattresses.setOnClickListener(this);
+        mBTNFurnishings.setOnClickListener(this);
+        mBTNDecor.setOnClickListener(this);
+        mBTNLightings.setOnClickListener(this);
+        mBTNModularFurniture.setOnClickListener(this);
+        mBTNClearanceSale.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnfurniture:
+                Intent intent = new Intent(DepartmentsActivity.this, FurnitureExtended.class);
+                startActivity(intent);
+                break;
+            case R.id.btnLiving:
+                Intent living = new Intent(DepartmentsActivity.this, LivingExtended.class);
+                startActivity(living);
+                break;
+            case R.id.btnBedRoom:
+                Intent bedroom = new Intent(DepartmentsActivity.this, BedroomExtended.class);
+                startActivity(bedroom);
+                break;
+            case R.id.btnKidsRoom:
+                Intent kidsroom = new Intent(DepartmentsActivity.this, KidsRoomExtended.class);
+                startActivity(kidsroom);
+                break;
+            case R.id.btnMattresses:
+                Intent mattresses = new Intent(DepartmentsActivity.this, MattressesExtended.class);
+                startActivity(mattresses);
+                break;
+            case R.id.btnFurnishings:
+                Intent furnishings = new Intent(DepartmentsActivity.this, FurnishingsExtended.class);
+                startActivity(furnishings);
+                break;
+            case R.id.btnDecor:
+                Intent decor = new Intent(DepartmentsActivity.this, DecorExtended.class);
+                startActivity(decor);
+                break;
+            case R.id.btnLightings:
+                Intent lightings = new Intent(DepartmentsActivity.this, LightingExtended.class);
+                startActivity(lightings);
+                break;
+            case R.id.btnModularFurniture:
+                Intent modularfurniture = new Intent(DepartmentsActivity.this, ModularFurnitureExtended.class);
+                startActivity(modularfurniture);
+                break;
+            case R.id.btnClearanceSale:
+                Intent clearancesale = new Intent(DepartmentsActivity.this, ClearanceSaleExtended.class);
+                startActivity(clearancesale);
+                break;
+        }
+        
+    }
+}
+
+//mintu
--- app/src/main/java/com/example/DepartmentSection/MattressesExtended.java
+++ app/src/main/java/com/example/DepartmentSection/MattressesExtended.java
@@ -1,16 +1,16 @@
-package com.example.pepperfry;
+package com.example.DepartmentSection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.pepperfry.R;
+
+public class MattressesExtended extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_mattresses_extended);
     }
 }
-
-//mintu
