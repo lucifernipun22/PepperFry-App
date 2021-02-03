50af470195f572189b4ed6c8cda5dea5b66e40a7
SpartanHarshad <harshadpawar8555@gmail.com>
2021-02-03 10:17:36 +0530

    Alert,Notification

---
+++ app/src/main/java/com/example/pepperfry/OrderRelatedFragment.java
@@ -0,0 +1,62 @@
+package com.example.pepperfry;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class OrderRelatedFragment extends Fragment {
+    private TextView mTvSignIn_Personal;
+    private ImageView mIvNotificationHoldingHand;
+    private Button mBtnSignIn;
+
+    public OrderRelatedFragment() {
+        // Required empty public constructor
+    }
+
+    public static OrderRelatedFragment newInstance() {
+        OrderRelatedFragment fragment = new OrderRelatedFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_order_related, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        intViewsAndListeners(view);
+    }
+
+    private void intViewsAndListeners(View view) {
+        mTvSignIn_Personal= view.findViewById(R.id.tvSign_inPersonal);
+        mIvNotificationHoldingHand = view.findViewById(R.id.ivnotificationIconHoldingHand);
+        mBtnSignIn = view.findViewById(R.id.btnSignIn);
+        mBtnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Got To Sign In Page
+            }
+        });
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        //ListenerContext
+    }
+}
---
+++ app/src/main/java/com/example/pepperfry/ViewPagerAdapter.java
@@ -0,0 +1,46 @@
+package com.example.pepperfry;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return AllNotificationFragment.newInstance();
+            case 1:
+                return OrderRelatedFragment.newInstance();
+        }
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String PageName = "ALL";
+        switch (position) {
+            case 0:
+                PageName = "ALL";
+                break;
+            case 1:
+                PageName = "ORDER RELATED";
+                break;
+        }
+        return PageName;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
