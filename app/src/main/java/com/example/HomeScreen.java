a62948684de7bf69c04ad3fcc1426e86396608ca
MINTU325 <sainimintu34@gmail.com>
2021-02-03 23:32:32 +0530

    Connection to Home Activity

--- app/src/main/java/com/example/HomeScreen.java
+++ app/src/main/java/com/example/HomeScreen.java
@@ -2,15 +2,29 @@ package com.example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.pepperfry.R;
 
 public class HomeScreen extends AppCompatActivity {
+    private ImageView mIVivDepartment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        mIVivDepartment = findViewById(R.id.ivDepartment);
+        mIVivDepartment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent(HomeScreen.this, DepartmentsActivity.class);
+                startActivity(intent3);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/LoginOrRegister.java
+++ app/src/main/java/com/example/LoginOrRegister.java
@@ -12,17 +12,26 @@ import com.example.pepperfry.R;
 public class LoginOrRegister extends AppCompatActivity {
 private Button mBTNLogin;
 private Button mBTNRegister;
+private Button mBTNSkipItKnow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_or_register);
         mBTNLogin = findViewById(R.id.btnLogin);
         mBTNRegister = findViewById(R.id.btnRegister);
+        mBTNSkipItKnow = findViewById(R.id.btnSkipItNow);
         mBTNRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginOrRegister.this, RegisterActivity.class);
                 startActivity(intent);
+            }
+        });
+        mBTNSkipItKnow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(LoginOrRegister.this, HomeScreen.class);
+                startActivity(intent2);
             }
         });
 
