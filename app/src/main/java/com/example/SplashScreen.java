17125c837a9263c43d98393e93321f3d769212e8
Nipun Jain <lucifernipun22@gmail.com>
2021-02-04 04:06:35 +0530

    "Intent"

--- app/src/main/java/com/example/SplashScreen.java
+++ app/src/main/java/com/example/SplashScreen.java
@@ -11,18 +11,37 @@ import android.widget.ImageView;
 
 import com.example.pepperfry.R;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class SplashScreen extends AppCompatActivity {
     Animation topAnim;
-    ImageView image;
+    Animation bottomAnim;
+    ImageView image1;
+    ImageView image2;
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash_screen);
+        timer =new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
         topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
-        image=findViewById(R.id.design);
-        image.setAnimation(topAnim);
+        bottomAnim=AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        image2=findViewById(R.id.design);
+        image2.setAnimation(bottomAnim);
+        image1=findViewById(R.id.logo);
+        image1.setAnimation(topAnim);
+
 
     }
 }
