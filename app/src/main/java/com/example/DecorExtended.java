b3ac0614c6aded067873e64b8c048cf0aff53a4e
Nipun Jain <lucifernipun22@gmail.com>
2021-02-03 19:40:29 +0530

    "App"

--- app/src/main/java/com/example/DecorExtended.java
+++ app/src/main/java/com/example/DecorExtended.java
@@ -1,18 +1,4 @@
 package com.example;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-
-import com.example.pepperfry.R;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-    }
+public class DecorExtended {
 }
