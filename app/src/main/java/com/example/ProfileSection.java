67ce446e08e846f96230b3efe1d204c459aec17d
MINTU325 <sainimintu34@gmail.com>
2021-02-04 23:01:05 +0530

    My Order activity_profile_extended_my_order.xml

--- app/src/main/java/com/example/ProfileSection.java
+++ app/src/main/java/com/example/ProfileSection.java
@@ -2,15 +2,26 @@ package com.example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.pepperfry.R;
 
 public class ProfileSection extends AppCompatActivity {
-
+private Button mBTNMyOrder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_section);
+        mBTNMyOrder=  findViewById(R.id.btnMyOrder);
+        mBTNMyOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileSection.this, ProfileExtendedMyOrder.class);
+                startActivity(intent);
+            }
+        });
     }
 }
