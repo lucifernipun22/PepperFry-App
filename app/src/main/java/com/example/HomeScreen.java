43def993ee0c2021b5269b4880e45702dd4452e7
MINTU325 <sainimintu34@gmail.com>
2021-02-04 09:23:29 +0530

    Connection to Home Activity

--- app/src/main/java/com/example/HomeScreen.java
+++ app/src/main/java/com/example/HomeScreen.java
@@ -12,12 +12,14 @@ import com.example.pepperfry.R;
 
 public class HomeScreen extends AppCompatActivity {
     private ImageView mIVivDepartment;
+    private ImageView mIVivAlert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
         mIVivDepartment = findViewById(R.id.ivDepartment);
+        mIVivAlert = findViewById(R.id.ivAlert);
         mIVivDepartment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,6 +27,14 @@ public class HomeScreen extends AppCompatActivity {
                 startActivity(intent3);
             }
         });
+        mIVivAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent4 = new Intent(HomeScreen.this, AlertsActivity.class);
+                startActivity(intent4);
+            }
+        });
+
 
     }
 }
