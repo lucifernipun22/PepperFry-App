9d52122aa245878c559f05dce7a9aba541553c9f
MINTU325 <sainimintu34@gmail.com>
2021-02-04 23:29:54 +0530

    TRack Order Activity

--- app/src/main/java/com/example/ProfileSection.java
+++ app/src/main/java/com/example/ProfileSection.java
@@ -11,16 +11,27 @@ import com.example.pepperfry.R;
 
 public class ProfileSection extends AppCompatActivity {
 private Button mBTNMyOrder;
+private Button mBTNTrackOrder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_section);
         mBTNMyOrder=  findViewById(R.id.btnMyOrder);
+        mBTNTrackOrder=  findViewById(R.id.btnTrackOrder);
         mBTNMyOrder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ProfileSection.this, ProfileExtendedMyOrder.class);
                 startActivity(intent);
+            }
+        });
+
+        mBTNTrackOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(ProfileSection.this, ProfileExtendedTrackOrder.class);
+                startActivity(intent2);
             }
         });
     }
